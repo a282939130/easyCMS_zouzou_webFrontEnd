5611aef254d9d8ecfdfdaf762934c74a4b51f62f
zouzou <262819080@qq.com>
2016-08-18 10:24:12 +0800

    ../

--- easyCMS/easyCMadd_addMember_modifyMember/modifyMember.js
+++ easyCMS/easyCMadd_addMember_modifyMember/modifyMember.js
@@ -1,15 +1,15 @@
 window.onload=function(){
 	var closeImg=document.getElementById("closeImg");
 	closeImg.onmouseover=function(){
-		closeImg.src="img/close1.png";
+		closeImg.src="../img/close1.png/";
 	};
 	closeImg.onmouseout=function(){
-		closeImg.src="img/close.png";
+		closeImg.src="../img/close.png/";
 	};
 	var MM_triger=document.getElementsByClassName("MM_triger")[0];
 	MM_triger.onclick=function(){
 		var parentInframe=parent.document.getElementById("secInframe");
-		parentInframe.src="memberManagement/userM_memberM_memberM.html";
+		parentInframe.src="memberManagement/userM_memberM_memberM.html/";
 	};
 
 
