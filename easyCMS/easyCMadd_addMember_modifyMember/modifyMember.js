23aafb3f228a5fae1fddd8b37175f2b5633544c3
zouzou <262819080@qq.com>
2016-08-21 20:00:57 +0800

    add the formValidate js

--- easyCMS/easyCMadd_addMember_modifyMember/modifyMember.js
+++ easyCMS/easyCMadd_addMember_modifyMember/modifyMember.js
@@ -7,9 +7,7 @@ window.onload=function(){
 	// 	closeImg.src="../img/close.png/";
 	// };
 	var MM_triger=document.getElementsByClassName("MM_triger")[0];
-	alert(MM_triger);
 	MM_triger.onclick=function(){
-		alert("www");
 		window.open("../memberManagement/userM_memberM_memberM.html","_self");
 	};
 
