149e609e384ef5ab888c57561a0749fa50bd9c14
zouzou <262819080@qq.com>
2016-08-24 17:44:53 +0800

    member manager delete row

--- easyCMS/memberManagement/js/memberM.js
+++ easyCMS/memberManagement/js/memberM.js
@@ -1,6 +1,10 @@
+var btn_del=function(obj){
+        	obj.parentNode.removeChild(obj);
+        }
 window.onload=function(){
         var btn_add=document.getElementById("btn_add");
         btn_add.onclick=function(){
                 window.open("../easyCMadd_addMember_modifyMember/modifyMember.html","_self");
         };
+
 };
