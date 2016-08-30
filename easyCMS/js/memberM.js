206e6229d50338eadb9678b81b3a732466f09548
a282939130 <282939130@qq.com>
2016-08-30 18:17:33 +0800

    test
    This is a test

--- easyCMS/js/memberM.js
+++ easyCMS/js/memberM.js
@@ -1,3 +1,4 @@
+//删除
 var btn_del=function(obj){
         	obj.parentNode.removeChild(obj);
         }
@@ -8,3 +9,19 @@ window.onload=function(){
         };
 
 };
+
+//复选框
+
+function check_box(isChecked){
+        //var chkbox = document.getElementsByTagName("input");
+        ////console.log(chkbox);
+        //if(chkbox.type == "checkbox"){
+        //        this.checked = isChecked;
+        //}
+        var inputs = document.getElementsByTagName("input");
+        for (var i = 0; i < inputs.length; i++) {
+                if (inputs[i].type == "checkbox") {
+                        inputs[i].checked = isChecked;
+                }
+        }
+}
